9db053ef16db5e469ba3755f7c0bca165efcf2ab
Alex Marandon <contact@alexmarandon.com>
2011-11-07 00:17:22 +0700

    Begin chapter 2

---
+++ scheme/section-2.1.scm
@@ -0,0 +1,46 @@
+#lang planet neil/sicp
+
+;(define (make-rat n d)
+;  (let ((g (gcd n d)))
+;    (cons (/ n g) (/ d g))))
+
+(define (numer x)
+  (car x))
+
+(define (denom x)
+  (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+; Exercise 2.1
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (< d 0)
+      (cons (/ (* n -1) g) (/ (* d -1) g))
+      (cons (/ n g) (/ d g)))))
+
