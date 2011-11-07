8810110512d37fada16b29a92a5fa39d84934d5f
Alex Marandon <contact@alexmarandon.com>
2011-11-07 17:55:04 +0700

    Exercise 2.2

--- scheme/section-2.1.scm
+++ scheme/section-2.1.scm
@@ -44,3 +44,38 @@
       (cons (/ (* n -1) g) (/ (* d -1) g))
       (cons (/ n g) (/ d g)))))
 
+; Exercise 2.2
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (avg a b)
+  (/ (+ a b) 2))
+
+(define (coord-average selector segment)
+  (avg (selector (start-segment segment)) (selector (end-segment segment))))
+
+(define (midpoint-segment segment)
+  (make-point (coord-average x-point segment)
+              (coord-average y-point segment)))
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
