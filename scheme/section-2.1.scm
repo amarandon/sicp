f9fceb1cb3f27eeffa74cf3e083bb58e724e5a58
Alex Marandon <contact@alexmarandon.com>
2011-11-09 19:19:38 +0700

    Exercise 2.4

--- scheme/section-2.1.scm
+++ scheme/section-2.1.scm
@@ -44,3 +44,24 @@
       (cons (/ (* n -1) g) (/ (* d -1) g))
       (cons (/ n g) (/ d g)))))
 
+
+; Section 2.1.3
+;(define (cons x y)
+;  (define (dispatch m)
+;    (cond ((= m 0) x)
+;          ((= m 1) y)
+;          (else (error "Argument not 0 or 1 -- CONS" m))))
+;  dispatch)
+
+;(define (car z) (z 0))
+;(define (cdr z) (z 1))
+
+; Exercise 2.4
+;(define (cons x y)
+;  (lambda (m) (m x y)))
+
+;(define (car z)
+;  (z (lambda (p q) p)))
+
+;(define (cdr z)
+;  (z (lambda (p q) q)))
