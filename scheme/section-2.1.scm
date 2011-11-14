20b46efe83d12f1e1339904123caa25a31294dd1
Alex Marandon <contact@alexmarandon.com>
2011-11-14 23:40:18 +0700

    Exercise 2.6

--- scheme/section-2.1.scm
+++ scheme/section-2.1.scm
@@ -127,3 +127,26 @@
 
 ;(define (cdr z)
 ;  (z (lambda (p q) q)))
+
+; Exercise 2.6
+(define zero 
+  (lambda (f)
+    (lambda (x) x)))
+
+(define one
+  (lambda (f)
+    (lambda (x) (f x))))
+
+(define two
+  (lambda (f) 
+    (lambda (x) (f (f x)))))
+
+(define (add-1 n)
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)))))
+
+(define (add n m)
+  (lambda (f)
+    (lambda (x)
+      ((m f) ((n f) x)))))
