9d71f073ee60cb1d89077146fd77e1e549cfce3f
Alex Marandon <contact@alexmarandon.com>
2011-11-18 22:11:04 +0700

    Exercise 2.12

--- scheme/section-2.1.scm
+++ scheme/section-2.1.scm
@@ -284,3 +284,23 @@
                 (>= up-y 0))
            ; [+, +] * [-, +]
            (make-interval (* up-x lo-y) (* up-x up-y))))))
+
+; Exercise 2.12
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (let ((w (/ (* c p) 
+              100)))
+    (make-center-width c w)))
+
+(define (percent i)
+  (let ((c (center i)))
+    (* (/ (width i) c) 
+       100)))
