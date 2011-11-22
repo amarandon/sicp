e8650e855ac782dce037631eb0fd0c2b5bdb3810
Alex Marandon <contact@alexmarandon.com>
2011-11-22 22:44:56 +0700

    Exercise 2.20

--- scheme/section-2.2.scm
+++ scheme/section-2.2.scm
@@ -14,3 +14,17 @@
       (iter (cdr l) (cons (car l) a))))
   (iter l nil))
 
+; Exercise 2.20
+(define (same-parity head . tail)
+  (let ((good? (if (odd? head)
+                 odd? 
+                 even?)))
+    (define (iter l a)
+      (if (null? l)
+        a
+        (iter (cdr l) (if (good? (car l))
+                        (cons (car l) a)
+                        a))))
+    (cons head
+          (reverse (iter tail nil)))))
+
