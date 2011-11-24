2c9afaf87796ede73d5567936036c188af950864
Alex Marandon <contact@alexmarandon.com>
2011-11-24 16:27:58 +0700

    Exercise 2.29a-c

--- scheme/section-2.2.scm
+++ scheme/section-2.2.scm
@@ -76,3 +76,54 @@
         ((not (pair? items)) (list items))
         (else (append (fringe (car items))
                       (fringe (cdr items))))))
+
+; Exercise 2.29
+(define (make-mobile left right) (list left right))
+
+(define (left-branch mobile) (car mobile))
+
+(define (right-branch mobile) (cadr mobile))
+
+(define (make-branch length structure) (list length structure))
+
+(define (branch-length branch) (car branch))
+
+(define (branch-structure branch) (cadr branch))
+
+(define (total-weight mobile)
+  (cond ((null? mobile) 0)
+        ((not (pair? mobile)) mobile)
+        (else (+ (total-weight (branch-structure (left-branch mobile)))
+                 (total-weight (branch-structure (right-branch mobile)))))))
+
+(define (branch-torque branch)
+  (* (branch-length branch)
+     (total-weight (branch-structure branch))))
+
+(define (balanced? mobile)
+  (or (not (pair? mobile))
+      (and (= (branch-torque (left-branch mobile))
+              (branch-torque (right-branch mobile)))
+           (balanced? (branch-structure (left-branch mobile)))
+           (balanced? (branch-structure (right-branch mobile))))))
+
+(define mobile1 ; unbalanced
+  (make-mobile
+    (make-branch 7 (make-mobile
+                     (make-branch 4 1)
+                     (make-branch 4 2)))
+    (make-branch 7 (make-mobile
+                     (make-branch 4 (make-mobile
+                                      (make-branch 3 3)
+                                      (make-branch 2 4)))
+                     (make-branch 4 5)))))
+
+(define mobile2 ; balanced
+  (make-mobile
+    (make-branch 2 (make-mobile
+                     (make-branch 8 2)
+                     (make-branch 4 4)))
+    (make-branch 4 (make-mobile
+                     (make-branch 3 2)
+                     (make-branch 6 1)))))
+
