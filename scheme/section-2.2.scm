e21c4ad3083a7acd07a4ff05372f9cdb1375a138
Alex Marandon <contact@alexmarandon.com>
2012-06-09 17:09:55 +0200

    Exercise 2.40

--- scheme/section-2.2.scm
+++ scheme/section-2.2.scm
@@ -212,7 +212,7 @@
            (matrix-*-vector cols row))
          m)))
 
-; Exercise 2.38
+; Exercise 2.38, 2.39
 (define (fold-left op initial seq)
   (define (iter result rest)
     (if (null? rest)
@@ -228,3 +228,53 @@
 
 (define (reverse-foldl seq)
   (fold-left (lambda (x y) (cons y x)) nil seq))
+
+; Exercise 2.40
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (next test-divisor)
+  (if (= test-divisor 2)
+      3
+      (+ test-divisor 2)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+
+(define (filter p s)
+  (cond ((null? s) nil)
+        ((p (car s))
+         (cons (car s) (filter p (cdr s))))
+        (else (filter p (cdr s)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap p s)
+  (accumulate append nil (map p s)))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum 
+       (filter prime-sum? (unique-pairs n))))
