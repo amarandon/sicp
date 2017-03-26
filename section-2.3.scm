c77a8f2b419861840f12a7668d455031a055b777
Alex Marandon <contact@alexmarandon.com>
2017-03-26 11:13:52 +0200

    Exercises 2.59, 2.60

--- section-2.3.scm
+++ section-2.3.scm
@@ -36,6 +36,8 @@
            (and (symbol? a) (symbol? b))
            (eq? a b))
          #t)
+        ((and (number? a) (number? b))
+         (eq? a b))
         ((and (null? a) (null? b))
          #t)
         ((and
@@ -133,4 +135,49 @@
           (error "unknown expression type -- DERIV" exp))))
 
 
-; Exercise 2.57
+; Section 2.3.3
+
+(define (element-of-set? x set)
+  (cond((null? set) false)
+       ((equal? x (car set)) true)
+       (else (element-of-set? x (cdr set)))))
+
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else 
+         (intersection-set (cdr set1) set2))))
+
+; Exercise 2.59
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else
+         (cons
+           (car set1)
+           (union-set (cdr set1) set2)))))
+
+
+; Exercise 2.60
+(define (dup-adjoin-set x set)
+    (cons x set))
+
+
+(define (dup-union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+         (cons
+           (car set1)
+           (dup-union-set (cdr set1) set2)))))
