a78564b4742e1e88ed9b8aa2ec4f5b7ce19214d8
Alex Marandon <contact@alexmarandon.com>
2017-03-27 04:08:11 +0200

    Exercise 2.62

--- section-2.3.scm
+++ section-2.3.scm
@@ -209,3 +209,19 @@
         ((< x (car set)) (cons x set))
         (else
           (cons (car set) (adjoin-ordered-set x (cdr set))))))
+
+; Exercise 2.62
+(define (union-ordered-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1
+                         (union-ordered-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1
+                         (union-ordered-set (cdr set1) set2)))
+                  ((< x2 x1)
+                   (cons x2
+                         (union-ordered-set (cdr set2) set1))))))))
