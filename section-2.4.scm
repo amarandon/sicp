883e480e8a605ce9cf05fe00cc67f6cf9797b8fc
Alex Marandon <contact@alexmarandon.com>
2017-04-09 10:16:01 +0200

    Exercise 2.77

--- section-2.4.scm
+++ section-2.4.scm
@@ -147,3 +147,46 @@
 ; differently from the code in the book so not 100% convincing easier.
 ; Anyway the good thing to take away from this exercise is the Expression
 ; Problem: http://wiki.c2.com/?ExpressionProblem
+
+; Exercise 2.77
+;
+; The z object is structured like this:
+;
+;   '(complex rectangular 3 4)
+;   
+; So the call to magnitude:
+;
+;   (magnitude z)
+;
+; Is equivalent to this:
+;
+;   (apply-generic 'magnitude '(complex rectangular 3 4))
+; 
+; apply-generic gets 'magnitude as it first argument and collects each of its
+; subsequent arguments in the args list. Here the args list contains only the z
+; object '(complex rectangular 3 4)
+; It calls type-tag on that object and gets the 'complex type tag.
+; It retrieves the procedure 'magnitude tagged with 'complex, which has been
+; registered by adding Alyssa's code to install-complex-package.
+; It applies this procedure to the content of the object, which has been
+; stripped from the 'complex tag but is still tagged 'rectangular.
+; So this is another call to the magnitude procedure which this time is equivalent to:
+;
+;   (apply-generic 'magnitude '(rectangular 3 4))
+;
+; apply-generic gets 'magnitude as it first argument and collects each of its
+; subsquent arguments in the args list. Here the args list contains the
+; object '(rectangular 3 4)
+; It calls type-tag on that object and gets the 'rectangular type tag.
+; It retrieves the procedure 'magnitude tagged with 'rectangular, which has been registered by install-rectangular-package.
+; It applies this procedure to the content of the object, which has been stripped from the all tags.
+; The procedure returns the car of the list representing the complex number.
+;
+; The magnitude procedure registered against the complex package is a generic
+; procedure whereas the magnitude registered against the rectangular package is
+; a concrete procedure that does actual work.
+;
+; apply-generic is applied twice, the first time it dispatches to the global
+; magnitude procedure, passing it the object stripped from the 'complex tag. 
+; The second time it dispatches to the internal magnitude procedure of the
+; rectangular package.
